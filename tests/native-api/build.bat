8b90d1e3ed1edc9e11106a3670075b3a2bc33c4e
Kandrashin Denis <mail@lintest.ru>
2021-06-23 21:44:06 +0300

    Заработали тесты

--- tests/native-api/build.bat
+++ tests/native-api/build.bat
@@ -1,11 +1,11 @@
 cmake -E make_directory build32w
 cd build32w
-cmake -A Win32 --build ..
-cmake --build . --config Release 
+cmake .. -A Win32 --build
+cmake . --build --config Release 
 cd ..
 
 cmake -E make_directory build64w
 cd build64w
-cmake -A x64 --build ..
-cmake --build . --config Release 
+cmake .. -A x64 --build
+cmake . --build --config Release 
 cd ..
